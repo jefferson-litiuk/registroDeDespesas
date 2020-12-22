0984eae1d9ff9cad7ef233d81414af668739331e
Jefferson Litiuk <jefferson.litiuk@hotmail.com>
2020-12-22 19:02:22 -0300

    Projeto finalizado

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ class Bd {
                 continue
             }
 
+            despesa.id = i
             despesas.push(despesa)
 
         }
@@ -102,6 +103,9 @@ class Bd {
         }
         return despesasFiltradas
 
+    }
+    remover(id) {
+        localStorage.removeItem(id)
     }
 
 }
@@ -193,6 +197,24 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
         linha.insertCell(1).innerHTML = d.tipo
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+
+        // Botão de exclusão
+        let btn = document.createElement("button")
+        btn.className = "btn btn-danger"
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function() {
+            //remover a despesa 
+            let id = this.id.replace('id_despesa_', '')
+
+            //alert(id)
+
+            bd.remover(id)
+            window.location.reload()
+        }
+    
+        linha.insertCell(4).append(btn)
+        console.log(d)
     });
 }
 function pesquisarDespesa() {
